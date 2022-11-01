5fbc0ff653009f5f4078dfbb1eb2fc35a7732d50
yuriybobyk <yuriybobyk4325@gmail.com>
2022-11-01 19:40:25 +0200

    HW1_JS done

---
+++ HW1/main.js
@@ -0,0 +1,45 @@
+let hello = 'hello'
+let owu = 'owu'
+let com = 'com'
+let ua = 'ua'
+let one = 1
+let ten = 10
+let nines = -999
+let ott = 123
+const PI = 3.14
+let shot = 2.7
+let big = 7>4
+let less = 7<4
+//
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(one)
+console.log(ten)
+console.log(nines)
+console.log(ott)
+console.log(PI)
+console.log(shot)
+console.log(big)
+console.log(less)
+//
+let firstName = 'Yurii'
+let middleName = 'Bobyk'
+let lastName = 'Volodymyrovych'
+console.log(`${firstName} ${middleName} ${lastName}`)
+//
+let a = 100
+let b = '100'
+let c = 7>4
+console.log(typeof (a))
+console.log(typeof (b))
+console.log(typeof (c))
+//
+let name = prompt('Your first name?')
+let lastname = prompt('Your last name?')
+let age = prompt('How old are you?')
+console.log(`${name} ${lastname} ${age}`)
+
+
+
