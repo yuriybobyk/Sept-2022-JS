953eaa37346fa5cb1bdaf18824a05f4212f7029b
yuriybobyk <yuriybobyk4325@gmail.com>
2022-11-21 19:58:01 +0200

    Some changes

--- HW6/main.js
+++ HW6/main.js
@@ -1,5 +1,13 @@
 // - Знайти та вивести довижину настипних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+let str1len = `hello world`
+console.log(str1len.length)
+
+let str2len = `lorem ipsum`
+console.log(str2len.length)
+
+let str3len = `javascript is cool`
+console.log(str3len.length)
 
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
