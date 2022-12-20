4091dee316ba976f4a25c30166c9f79f5b456539
yuriybobyk <yuriybobyk4325@gmail.com>
2022-12-20 12:05:37 +0200

    some changes

--- mini-project/post-details.js
+++ mini-project/post-details.js
@@ -13,6 +13,25 @@ for (const item in parse) {
     infodiv.innerText = `${item} -- ${parse[item]}`
 }
 
-
+const commenturl = parse.id + '/comments'
+fetch('https://jsonplaceholder.typicode.com/posts/' + commenturl)
+    .then(response => response.json())
+    .then(response =>{
+        const commentbtn = document.createElement('button')
+        commentbtn.classList.add('commentbtn')
+        commentbtn.innerText = 'Comments to post'
+        detailcontainer.appendChild(commentbtn)
+        commentbtn.onclick = function (){
+            const commentdiv = document.createElement('div')
+            commentdiv.classList.add('commentdiv')
+            document.body.appendChild(commentdiv)
+            for (const element of response) {
+                const commentdesc = document.createElement('div')
+                commentdesc.classList.add('commentdesc')
+                commentdesc.innerText = `${element.postId} -- ${element.body}`
+                commentdiv.appendChild(commentdesc)
+            }
+        }
+    })
 
 
