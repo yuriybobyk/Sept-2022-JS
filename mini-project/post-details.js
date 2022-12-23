47b76514670b093416b13d7c956d2205d43b312c
yuriybobyk <yuriybobyk4325@gmail.com>
2022-12-23 13:44:02 +0200

    done

--- mini-project/post-details.js
+++ mini-project/post-details.js
@@ -17,20 +17,24 @@ const commenturl = parse.id + '/comments'
 fetch('https://jsonplaceholder.typicode.com/posts/' + commenturl)
     .then(response => response.json())
     .then(response =>{
+        const buttondiv = document.createElement('div')
+        buttondiv.classList.add('buttondiv')
+        detailcontainer.appendChild(buttondiv)
         const commentbtn = document.createElement('button')
         commentbtn.classList.add('commentbtn')
         commentbtn.innerText = 'Comments to post'
-        detailcontainer.appendChild(commentbtn)
+        buttondiv.appendChild(commentbtn)
         commentbtn.onclick = function (){
             const commentdiv = document.createElement('div')
             commentdiv.classList.add('commentdiv')
-            document.body.appendChild(commentdiv)
+            detailcontainer.appendChild(commentdiv)
             for (const element of response) {
                 const commentdesc = document.createElement('div')
                 commentdesc.classList.add('commentdesc')
                 commentdesc.innerText = `${element.postId} -- ${element.body}`
                 commentdiv.appendChild(commentdesc)
             }
+            commentbtn.disabled = 'true'
         }
     })
 
